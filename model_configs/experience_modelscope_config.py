78f7019854abcab15bf4ff8a145b04da2b05692d
lk <c1824503917@outlook.com>
2025-09-14 16:54:37 +0800

    feat: 新增模型配置 新增Qwen/Qwen3-Next-80B-A3B-Instruct和Qwen/Qwen3-Next-80B-A3B-Thinking模型配置

--- model_configs/experience_modelscope_config.py
+++ model_configs/experience_modelscope_config.py
@@ -18,7 +18,9 @@ CLIENT_PARAMS = {
 model_list = ["Qwen/Qwen3-235B-A22B-Instruct-2507", "Qwen/Qwen3-30B-A3B-Instruct-2507", "MiniMax/MiniMax-M1-80k",
               "ZhipuAI/GLM-4.5",
               # "moonshotai/Kimi-K2-Instruct"
-              "moonshotai/Kimi-K2-Instruct-0905"
+              "moonshotai/Kimi-K2-Instruct-0905",
+              "Qwen/Qwen3-Next-80B-A3B-Instruct",
+              "Qwen/Qwen3-Next-80B-A3B-Thinking"
               ]
 model_choice = random.choice(model_list)
 
